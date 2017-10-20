d636467bba17585b558ec5f60ae019689fba0458
yogna <yogna@YONATAN-LAPTOP>
2017-10-20 12:02:07 +0300

    First commit

---
+++ University/src/testing/Lesson_1.java
@@ -0,0 +1,20 @@
+package testing;
+
+public class Lesson_1 {
+
+	public static void main(String[] args) {
+		new Lesson_1();
+	}
+
+	public Lesson_1() {
+		MyName("Yonatan");
+	}
+
+	public void MyName(String name)
+	{
+		System.out.println("*********");
+		System.out.printf("*%s*\n", name);
+		System.out.println("*********");
+	}
+
+}
