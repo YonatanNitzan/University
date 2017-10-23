08688d7177485cf4458fd7ba13b0a3e4e288e664
yogna <yogna@YONATAN-LAPTOP>
2017-10-23 10:56:47 +0300

    asdmjlkas;ld

--- University/src/assignment11/Line.java
+++ University/src/assignment11/Line.java
@@ -10,20 +10,27 @@ public class Line {
 
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Please enter 4 integers ");
+		
+		/*Receiving the first dot's coordinates*/
 		System.out.print("Enter the x coordinates of the first dot: ");
 		x1 = scan.nextInt();
 		System.out.print("Enter the y coordinates of the first dot: ");
 		y1 = scan.nextInt();
+		
+		/*Receiving the first dot's coordinates*/
 		System.out.print("Enter the x coordinates of the second dot: ");
 		x2 = scan.nextInt();
 		System.out.print("Enter the y coordinates of the second dot: ");
 		y2 = scan.nextInt();
-
+		
+		/*Calculates the distance*/
 		distance = Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
 
+		/*Prints the dots and the distance*/
 		System.out.printf("\nThe length of the line between the points"
 				+ " (%d,%d) and (%d,%d) is %f", x1, y1, x2, y2, distance);
-
+		
 		scan.close();
-	}
-}
+	}//End of main
+	
+}//End of class
--- University/src/assignment11/Trapezoid.java
+++ University/src/assignment11/Trapezoid.java
@@ -6,29 +6,34 @@ public class Trapezoid {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		
+		/*Receives the information of the first base*/
 		System.out.println("Please enter the left point coordinates "
 				+ "of the base followed by its length:");
 		int x1 = scan.nextInt();
 		int y1 = scan.nextInt();
 		int base1 = scan.nextInt();
+		
+		/*Receives the information of the second base*/
 		System.out.println("Please enter the left point coordinates "
 				+ "of the other base followed by its length:");
 		int x2 = scan.nextInt();
 		int y2 = scan.nextInt();
 		int base2 = scan.nextInt();
 		
-		double area, perimeter;
-
+		/*Calculates the legs of the trapezoid*/
 		double leg1 = Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
 		double leg2 = Math.sqrt(Math.pow(((x1 + base1) - (x2 + base2)), 2) + Math.pow((y1 - y2), 2));
 		
-		area = ((Math.abs(y1-y2) * (base1 + base2))/2);
+		/*Calculations*/
+		double area = ((double)(Math.abs(y1-y2) * (base1 + base2))/2);
+		double perimeter = (base1 + base2 + leg1 + leg2);
 		
-		perimeter = (base1 + base2 + leg1 + leg2);
-		
+		/*Prints results*/
 		System.out.print("The area of the trapezoid is: " + area);
 		System.out.print("\nThe perimeter of the trapezoid is: " + perimeter);
 		
 		scan.close();
-	}
-}
+	}//End of main
+	
+}//End of class
